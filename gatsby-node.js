3f3da624458e38a1396507feb5054f0a65f281e2
Dustin Schau <DSchau@users.noreply.github.com>
2021-05-19 16:33:06 -0700

    feat: add ui to front the backend (of course) (#2)
    Co-authored-by: Ward Peeters <ward@coding-tech.com>

--- gatsby-node.js
+++
@@ -1,7 +0,0 @@
-exports.onCreatePage = ({ actions }) => {
-  actions.createPage({
-    path: `/api/ssr/`,
-    component: require.resolve("./src/dummy.js"),
-    matchPath: "/trickster/",
-  });
-};
