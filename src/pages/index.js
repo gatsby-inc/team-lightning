d7531f3f0e5a078ab217886d86a8e8d3289ee164
Ward Peeters <ward@coding-tech.com>
2021-05-19 20:15:58 +0200

    add radio buttons & og code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,45 +1,93 @@
-import * as React from "react"
-import debounce from 'debounce';
+import * as React from "react";
+import debounce from "debounce";
 
-import { Copy } from '../components/copy/copy'
+import { Copy } from "../components/copy/copy";
 
-import * as styles from './index.module.css'
+import * as styles from "./index.module.css";
 
-const getUrl = version => {
-  let base = `https://teamlightning.gatsbyjs.io`
-  if (process.env.NODE_ENV === 'development') {
-    base = `http://localhost:8000`
+const getUrl = (version, social) => {
+  let base = `https://teamlightning.gatsbyjs.io`;
+  if (process.env.NODE_ENV === "development") {
+    base = `http://localhost:8000`;
   }
-  return `${base}/api/social-card?text=${version}`
-}
-
+  return `${base}/api/social-card?text=${version}&social=${social}`;
+};
 
 // markup
 const IndexPage = () => {
-  const [version, setVersion] = React.useState('')
-  const [imageVersion, setImageVersion] = React.useState('')
-  const url = getUrl(version)
+  const [version, setVersion] = React.useState("");
+  const [activeSocial, setActiveSocial] = React.useState("twitter");
+  const [imageVersion, setImageVersion] = React.useState("");
+  const url = getUrl(version, activeSocial);
 
-  const requestImage = React.useCallback(debounce((value) => {
-    setImageVersion(value)
-  }, 300), []);
+  const requestImage = React.useCallback(
+    debounce((value) => {
+      setImageVersion(value);
+    }, 300),
+    []
+  );
 
-  const onChange = React.useCallback(e => {
-    setVersion(e.target.value)
-    requestImage(e.target.value)
-  }, [])
-  
+  const onChange = React.useCallback((e) => {
+    setVersion(e.target.value);
+    requestImage(e.target.value);
+  }, []);
+
+  const selectSocial = React.useCallback((e) => {
+    setActiveSocial(e.target.value);
+  }, []);
+
   return (
     <main className={styles.main}>
-        <input className={styles.input} placeholder="Input a version number (e.g. 3.5)" onChange={onChange} />
-        <div className={styles.url}>
-          <input className={[styles.input].concat(styles.urlInput).join(' ')} readOnly={true} value={url} />
-          <Copy className={styles.copy} content={url} />
-        </div>
-        {/* <input type="submit" className={styles.button} /> */}
-        <img className={styles.image} src={`/api/social-card?text=${imageVersion}`} />
-    </main>
-  )
-}
+      <input
+        className={styles.input}
+        placeholder="Input a version number (e.g. 3.5)"
+        onChange={onChange}
+      />
+      <div>
+        <input
+          type="radio"
+          id="social-twitter"
+          name="social"
+          value="twitter"
+          selected={activeSocial === "twitter"}
+          onChange={selectSocial}
+        />
+        <label htmlFor="social-twitter" className={styles.pointer}>Twitter</label>
 
-export default IndexPage
+        <input
+          type="radio"
+          id="social-linkedin"
+          name="social"
+          value="linkedin"
+          selected={activeSocial === "linkedin"}
+          onChange={selectSocial}
+        />
+        <label htmlFor="social-linkedin" className={styles.pointer}>LinkedIn</label>
+      </div>
+      <div className={styles.url}>
+        <input
+          className={[styles.input].concat(styles.urlInput).join(" ")}
+          readOnly={true}
+          value={url}
+        />
+        <Copy className={styles.copy} content={url} />
+      </div>
+      {/* <input type="submit" className={styles.button} /> */}
+      <img
+        className={styles.image}
+        src={`/api/social-card?text=${imageVersion}`}
+      />
+
+      <fieldset>
+      <legend>OG Code</legend>
+        <pre
+          dangerouslySetInnerHTML={{
+            __html: `&lt;meta property="og:image" content="${url}" /&gt;`,
+          }}
+        />
+      </fieldset>
+    </main>
+  );
+};
+
+export default IndexPage;
