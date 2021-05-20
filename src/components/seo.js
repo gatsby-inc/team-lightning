d552408a5b58a087986b227e1de428d211c507db
Dustin Schau <DSchau@users.noreply.github.com>
2021-05-20 10:34:00 -0700

    feat: implement ui (#6)

---
+++ src/components/seo.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+function SEO({ title, image, description, url }) {
+  return (
+    <Helmet
+      title={title}
+      titleTemplate={`%s | Release Notes Generator`}
+      meta={[
+        {
+          name: `description`,
+          content: description,
+        },
+
+        /*
+          @todo Schema.org tags
+          @see https://github.com/jlengstorf/lengstorf.com/blob/master/src/components/SEO.js
+        */
+
+        /* OpenGraph tags */
+        {
+          property: `og:url`,
+          content: url,
+        },
+
+        {
+          property: `og:title`,
+          content: title,
+        },
+        {
+          property: `og:description`,
+          content: description,
+        },
+        {
+          name: `twitter:creator`,
+          content: '@gatsbyjs',
+        },
+        {
+          name: `twitter:title`,
+          content: title,
+        },
+        {
+          name: `twitter:description`,
+          content: description,
+        },
+      ].concat(image ? [
+        {
+          name: `image`,
+          content: image,
+        },
+        {
+          property: `og:image`,
+          content: image,
+        },
+
+        /* Twitter Card tags */
+        {
+          name: `twitter:card`,
+          content: `summary_large_image`,
+        },
+      ] : [])}
+    />
+  )
+}
+
+export { SEO }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,28 @@
 import * as React from "react";
 import debounce from "debounce";
+import { Link } from 'gatsby'
+import slugify from "@sindresorhus/slugify";
 
 import { Copy } from "../components/copy/copy";
+import { Header } from "../components/header"
+import { SEO } from "../components/seo"
 
 import * as styles from "./index.module.css";
 
-const getUrl = (version, social) => {
+const getUrl = (version, format) => {
   let base = `https://teamlightning.gatsbyjs.io`;
   if (process.env.NODE_ENV === "development") {
     base = `http://localhost:8000`;
   }
-  return `${base}/api/social-card?text=${version}&social=${social}`;
+  return `${base}/api/social-card?text=${version}&format=${format}`;
 };
 
 // markup
 const IndexPage = () => {
   const [version, setVersion] = React.useState("");
-  const [activeSocial, setActiveSocial] = React.useState("twitter");
-  const [imageVersion, setImageVersion] = React.useState("");
-  const url = getUrl(version, activeSocial);
+  const [activeFormat, setActiveFormat] = React.useState("landscape");
+  const [imageVersion, setImageVersion] = React.useState(" todo ");
+  const url = getUrl(version, activeFormat);
 
   const requestImage = React.useCallback(
     debounce((value) => {
@@ -32,74 +36,95 @@ const IndexPage = () => {
     requestImage(e.target.value);
   }, []);
 
-  const selectSocial = React.useCallback((e) => {
-    setActiveSocial(e.target.value);
+  const selectFormat = React.useCallback((e) => {
+    setActiveFormat(e.target.value);
   }, []);
 
   return (
-    <main className={styles.main}>
-      <div className={styles.uiWrapper}>
-        <div>
-          <label htmlFor="imageText">Text</label>
-          <input
-            className={styles.input}
-            placeholder="Input a version number (e.g. 3.5)"
-            onChange={onChange}
-            id="imageText"
-          />
-        </div>
-        <fieldset className={styles.format}>
-          <legend>Format</legend>
-          <input
-            type="radio"
-            id="social-twitter"
-            name="social"
-            value="twitter"
-            selected={activeSocial === "twitter"}
-            onChange={selectSocial}
-          />
-          <label htmlFor="social-twitter" className={styles.pointer}>
-            Twitter
-          </label>
-          <input
-            type="radio"
-            id="social-linkedin"
-            name="social"
-            value="linkedin"
-            selected={activeSocial === "linkedin"}
-            onChange={selectSocial}
-          />
-          <label htmlFor="social-linkedin" className={styles.pointer}>
-            LinkedIn
-          </label>
-        </fieldset>
-      </div>
-      <div className={styles.resultWrapper}>
-        <img
-          className={styles.image}
-          src={`/api/social-card?text=${imageVersion}`}
-        />
-        <div className={styles.url}>
-          <input
-            className={[styles.input].concat(styles.urlInput).join(" ")}
-            readOnly={true}
-            value={url}
-          />
-          <Copy className={styles.copy} content={url} />
-        </div>
-        {/* <input type="submit" className={styles.button} /> */}
-        <fieldset className={styles.ogCode}>
-          <legend>OG Code</legend>
-          <div className={styles.overflow}>
-            <pre
-              dangerouslySetInnerHTML={{
-                __html: `&lt;meta property="og:image" content="${url}" /&gt;`,
-              }}
+    <div>
+      <SEO title="Release Notes Generator"
+        description="Gatsby's release notes generator to generate our social sharing cards, powered by Gatsby Cloud Functions"
+        image="/api/social-card?text=Release%20Notes%20Generator&format=landscape"
+      />
+      <Header />
+      <main className={styles.main}>
+        <div className={styles.uiWrapper}>
+          <div>
+            <label htmlFor="imageText">Text</label>
+            <input
+              className={styles.input}
+              placeholder="Input a version number (e.g. 3.5)"
+              onChange={onChange}
+              id="imageText"
             />
           </div>
-        </fieldset>
-      </div>
-    </main>
+          <fieldset className={styles.format}>
+            <legend>Format</legend>
+            <input
+              type="radio"
+              id="landscape"
+              name="format"
+              value="landscape"
+              selected={activeFormat === "twitter"}
+              onChange={selectFormat}
+            />
+            <label htmlFor="landscape" className={styles.pointer}>
+              Landscape
+            </label>
+            <input
+              type="radio"
+              id="format-square"
+              name="format"
+              value="square"
+              selected={activeFormat === "square"}
+              onChange={selectFormat}
+            />
+            <label htmlFor="format-square" className={styles.pointer}>
+              Square
+            </label>
+          </fieldset>
+          <div className={styles.description}>
+            <h3>What is this?</h3>
+            <p>This service generates images that you can use as <code>og:image</code>s to use with the corresponding &lt;meta&gt; tags.</p>
+            <p>It's using Gatsby Cloud Functions under the hood.</p>
+            <p>Find out how this works and deploy your own image generator by visiting GitHub.</p>
+          </div>
+          <div className={styles.description}>
+            <h3>How was this built?</h3>
+            <p><a href="https://gatsbyjs.com/functions">Gatsby Cloud Functions</a>. Care about the details? We know you do, check 'em out the <Link to="/how/">full details</Link>.</p>
+            <p>It's using Gatsby Cloud Functions under the hood.</p>
+            <p>Find out how this works and deploy your own image generator by visiting GitHub.</p>
+          </div>
+        </div>
+        <div className={styles.resultWrapper}>
+          <img
+            className={styles.image}
+            src={`/api/social-card?text=${imageVersion}&format=${activeFormat}`}
+          />
+          <div className={styles.url}>
+            <input
+              className={[styles.input].concat(styles.urlInput).join(" ")}
+              readOnly={true}
+              value={url}
+            />
+            <Copy className={styles.copy} content={url} />
+          </div>
+
+          <a href={`/api/social-card?text=${imageVersion}`} download={`release-${slugify(imageVersion)}.png`}>download me</a>
+          {/* <input type="submit" className={styles.button} /> */}
+          <fieldset className={styles.ogCode}>
+            <legend>OG Code</legend>
+            <div className={styles.overflow}>
+              <pre
+                dangerouslySetInnerHTML={{
+                  __html: `&lt;meta property="og:image" content="${url}" /&gt;`,
+                }}
+              />
+            </div>
+          </fieldset>
+        </div>
+      </main>
+    </div>
   );
 };
 
