8ea5c5546d170c59451b080608433abacdc5bf3a
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2021-05-20 22:48:06 +0530

    Add a square variant

--- src/api/social-card.js
+++ src/api/social-card.js
@@ -3,43 +3,46 @@ import Jimp from "jimp";
 import * as yup from "yup";
 
 const schema = yup.object().shape({
-  // image: yup.string().required(),
-  version: yup.string().required(),
-  // text: yup.array().of(yup.object().shape({
-  //   x: yup.number(),
-  //   y: yup.number(),
-  //   center: yup.bool(),
-  //   value: yup.string().required(),
-  //   font: yup.number().required()
-  // })).min(1),
-  // type: yup.string().default(Jimp.MIME_PNG)
+  text: yup.string().required(),
+  format: yup.string().required(),
 });
 
-// const positionText = (textObj, image, font) => {
-//   const [width, height] = [Jimp.measureText, Jimp.measureTextHeight].map(method => method.bind(Jimp)(font, textObj.value))
+const LANDSCAPE_FORMAT = {
+  background: "landcape-template.png",
+  font: "Inter-ExtraBold.ttf.fnt",
+  textY: 380,
+};
 
-//   return image.print(
-//     font,
-//     textObj.center ? (image.getWidth() / 2) - (width / 2) : x,
-//     textObj.center ? (image.getHeight() / 2) - (height / 2) : y,
-//   )
-// }
+const SQUARE_FORMAT = {
+  background: "square-template.png",
+  font: "Inter-ExtraBold.ttf.fnt",
+  textY: 780,
+};
 
-const IMAGE_TEMPLATE_URL =
+const HOST =
   process.env.NODE_ENV === "production"
-    ? "https://teamlightning.gatsbyjs.io/gatsby-release.png"
-    : "http://localhost:8000/gatsby-release.png";
+    ? "https://teamlightning.gatsbyjs.io/"
+    : "http://localhost:8000/";
 
 export default async function socialCard(req, res) {
   try {
-    // const { image, text = [], type } = await schema.validate(req.query || req.body)
-    const { version } = await schema.validate(req.query);
-    const imageRes = await fetch(IMAGE_TEMPLATE_URL);
-    const imageBuffer = await imageRes.buffer();
+    const { text, format } = await schema.validate(req.query);
 
-    const font = await Jimp.loadFont(
-      "https://raw.githubusercontent.com/oliver-moran/jimp/master/packages/plugin-print/fonts/open-sans/open-sans-128-black/open-sans-128-black.fnt"
-    );
+    let options;
+    if (format === `landscape`) {
+      options = LANDSCAPE_FORMAT;
+    } else if (format === `square`) {
+      options = SQUARE_FORMAT;
+    } else {
+      return res.status(500).json({
+        message: `You need to pass in a format`,
+      });
+    }
+
+    const font = await Jimp.loadFont(`${HOST}${options.font}`);
+
+    const imageRes = await fetch(`${HOST}${options.background}`);
+    const imageBuffer = await imageRes.buffer();
 
     let modifiedImage = await Jimp.read(imageBuffer);
 
@@ -48,39 +51,23 @@ export default async function socialCard(req, res) {
       modifiedImage.getHeight(),
     ];
     const textDimensions = [
-      Jimp.measureText(font, version),
-      Jimp.measureTextHeight(font, version),
+      Jimp.measureText(font, text),
+      Jimp.measureTextHeight(font, text),
     ];
 
     modifiedImage.print(
       font,
-      image.getWidth() / 2 - width / 2,
-      textObj.center ? image.getHeight() / 2 - height / 2 : y
+      imageDimensions[0] / 2 - textDimensions[0] / 2,
+      // This is approximate
+      options.textY,
+      text
     );
-
-    // const sample = [
-    //   {
-    //     value: 'v3.5',
-    //     center: true
-    //   }
-    // ]
-
-    // modifiedImage.print(
-    //   font,
-    //   textObj.center ? (image.getWidth() / 2) - (width / 2) : x,
-    //   textObj.center ? (image.getHeight() / 2) - (height / 2) : y,
-    // )
-
-    // for (let block of sample) {
-    //   modifiedImage = positionText(block, modifiedImage, font)
-    // }
 
     return res
       .header("Content-Type", "image/png")
       .status(200)
-      .send(await modifiedImage.getBufferAsync(type));
+      .send(await modifiedImage.getBufferAsync(Jimp.MIME_PNG));
   } catch (e) {
-    // TODO: better fallback? Fallback to just image? etc.
     return res.status(500).json({
       message: e.message,
       stack: e.stack,
