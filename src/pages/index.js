91225c8405690f78d02e74d59b22f83da1b29e1e
Florian Kissling <21834+fk@users.noreply.github.com>
2021-05-20 23:04:26 +0200

    feat: UI one more time (#8)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import * as React from "react";
 import debounce from "debounce";
-import { Link } from 'gatsby'
+import { Link } from "gatsby";
 import slugify from "@sindresorhus/slugify";
 
 import { Copy } from "../components/copy/copy";
-import { Header } from "../components/header"
-import { Seo } from "../components/seo"
+import { Header } from "../components/header";
+import { Seo } from "../components/seo";
 
 import * as styles from "./index.module.css";
 
@@ -42,7 +42,8 @@ const IndexPage = () => {
 
   return (
     <div>
-      <Seo title="Release Notes Generator"
+      <Seo
+        title="Release Notes Generator"
         description="Gatsby's release notes generator to generate our social sharing cards, powered by Gatsby Cloud Functions"
         image="/api/social-card?text=Release%20Notes%20Generator&format=landscape"
       />
@@ -53,47 +54,52 @@ const IndexPage = () => {
             <label htmlFor="imageText">Text</label>
             <input
               className={styles.input}
-              placeholder="Input a version number (e.g. 3.5)"
+              placeholder='E. g. "v3.5"'
               onChange={onChange}
               id="imageText"
+              name="imageText"
             />
           </div>
           <fieldset className={styles.format}>
             <legend>Format</legend>
-            <input
-              type="radio"
-              id="landscape"
-              name="format"
-              value="landscape"
-              selected={activeFormat === "landscape"}
-              onChange={selectFormat}
-            />
-            <label htmlFor="landscape" className={styles.pointer}>
+            <label className={styles.pointer}>
+              <input
+                type="radio"
+                id="landscape"
+                name="format"
+                value="landscape"
+                selected={activeFormat === "landscape"}
+                onChange={selectFormat}
+              />
               Landscape
             </label>
-            <input
-              type="radio"
-              id="format-square"
-              name="format"
-              value="square"
-              selected={activeFormat === "square"}
-              onChange={selectFormat}
-            />
-            <label htmlFor="format-square" className={styles.pointer}>
+            <label className={styles.pointer}>
+              <input
+                type="radio"
+                id="format-square"
+                name="format"
+                value="square"
+                selected={activeFormat === "square"}
+                onChange={selectFormat}
+              />
               Square
             </label>
           </fieldset>
           <div className={styles.description}>
-            <h3>What is this?</h3>
-            <p>This service generates images that you can use as <code>og:image</code>s to use with the corresponding &lt;meta&gt; tags.</p>
-            <p>It's using Gatsby Cloud Functions under the hood.</p>
-            <p>Find out how this works and deploy your own image generator by visiting GitHub.</p>
-          </div>
-          <div className={styles.description}>
-            <h3>How was this built?</h3>
-            <p><a href="https://gatsbyjs.com/functions">Gatsby Cloud Functions</a>. Care about the details? We know you do, check 'em out the <Link to="/how/">full details</Link>.</p>
-            <p>It's using Gatsby Cloud Functions under the hood.</p>
-            <p>Find out how this works and deploy your own image generator by visiting GitHub.</p>
+            <h2 className={styles.heading}>What is this?</h2>
+            <p>
+              This service generates social media images that can be used as{" "}
+              <code>og:image</code> along the corresponding{" "}
+              <code>&lt;meta&gt;</code> tags. It's using{" "}
+              <a href="https://gatsbyjs.com/functions">
+                Gatsby Cloud Functions
+              </a>{" "}
+              under the hood.
+            </p>
+            <p>
+              <Link to="/how/">Learn more about the details</Link> and deploy
+              your own image generator by visiting GitHub.
+            </p>
           </div>
         </div>
         <div className={styles.resultWrapper}>
@@ -101,6 +107,7 @@ const IndexPage = () => {
             className={styles.image}
             src={`/api/social-card?text=${imageVersion}&format=${activeFormat}`}
           />
+
           <div className={styles.url}>
             <input
               className={[styles.input].concat(styles.urlInput).join(" ")}
@@ -109,9 +116,13 @@ const IndexPage = () => {
             />
             <Copy className={styles.copy} content={url} />
           </div>
-
-          <a href={`/api/social-card?text=${imageVersion}`} download={`release-${slugify(imageVersion)}.png`} className={[styles.button].concat(styles.download).join(' ')}>Download Assets</a>
-          {/* <input type="submit" className={styles.button} /> */}
+          <a
+            href={`/api/social-card?text=${imageVersion}`}
+            download={`release-${slugify(imageVersion)}.png`}
+            className={styles.button}
+          >
+            Download image
+          </a>
           <fieldset className={styles.ogCode}>
             <legend>OG Code</legend>
             <div className={styles.overflow}>
