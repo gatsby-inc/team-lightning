418a3f2492a0f6973496e2823faa801f20100c24
Dustin Schau <dustinschau@gmail.com>
2021-05-20 16:07:19 -0700

    chore: highligh

--- src/components/highlight.js
+++ src/components/highlight.js
@@ -3,6 +3,9 @@ import * as React from "react";
 import Highlight, { defaultProps } from "prism-react-renderer";
 import Okadia from "prism-react-renderer/themes/okaidia";
 
+import { Copy } from './copy/copy'
+import * as styles from './highlight.module.css'
+
 function Highlighter({ code, language = "js", ...props }) {
   return (
     <Highlight
@@ -13,22 +16,21 @@ function Highlighter({ code, language = "js", ...props }) {
       {...props}
     >
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre
-          className={className}
-          style={{
-            ...style,
-            padding: `var(--space-2xl)`,
-            borderRadius: `var(--radius-md)`,
-          }}
-        >
-          {tokens.map((line, i) => (
-            <div {...getLineProps({ line, key: i })}>
-              {line.map((token, key) => (
-                <span {...getTokenProps({ token, key })} />
-              ))}
-            </div>
-          ))}
-        </pre>
+        <div className={styles.highlight}>
+          <pre
+            className={[className].concat(styles.pre).join(' ')}
+            style={style}
+          >
+            <Copy className={styles.copy} />
+            <code className={styles.code}>{tokens.map((line, i) => (
+                <div {...getLineProps({ line, key: i })}>
+                  {line.map((token, key) => (
+                    <span {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
+              ))}</code>
+          </pre>
+        </div>
       )}
     </Highlight>
   );
