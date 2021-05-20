55ab287b940a2ff46aa5d7895d1794d34dc96b7c
Florian Kissling <florian.kissling@gmail.com>
2021-05-20 21:51:36 +0200

    Add `header` to `/how` page; format
    Could also be in `layout`.

--- src/pages/how.js
+++ src/pages/how.js
@@ -1,35 +1,46 @@
-import * as React from 'react'
+import * as React from "react";
 
 import Highlight, { defaultProps } from "prism-react-renderer";
-import Okadia from 'prism-react-renderer/themes/okaidia';
-import text from '!!raw-loader!../api/social-card.js'
+import Okadia from "prism-react-renderer/themes/okaidia";
+import text from "!!raw-loader!../api/social-card.js";
 
-import {main } from "./index.module.css";
-import * as styles from './how.module.css'
+import { main } from "./index.module.css";
+import { Header } from "../components/header";
+
+import * as styles from "./how.module.css";
 
 function How() {
   return (
-    <main className={main}>
-    <div>test</div>
-    <div>
-      <h1>How</h1>
-      <h2>Powered by Gatsby Cloud Functions</h2>
-      <Highlight {...defaultProps} theme={Okadia} className={styles.code} code={text} language="js">
-        {({ className, style, tokens, getLineProps, getTokenProps }) => (
-          <pre className={className} style={style}>
-            {tokens.map((line, i) => (
-              <div {...getLineProps({ line, key: i })}>
-                {line.map((token, key) => (
-                  <span {...getTokenProps({ token, key })} />
+    <>
+      <Header />
+      <main className={main}>
+        <div>test</div>
+        <div>
+          <h1>How</h1>
+          <h2>Powered by Gatsby Cloud Functions</h2>
+          <Highlight
+            {...defaultProps}
+            theme={Okadia}
+            className={styles.code}
+            code={text}
+            language="js"
+          >
+            {({ className, style, tokens, getLineProps, getTokenProps }) => (
+              <pre className={className} style={style}>
+                {tokens.map((line, i) => (
+                  <div {...getLineProps({ line, key: i })}>
+                    {line.map((token, key) => (
+                      <span {...getTokenProps({ token, key })} />
+                    ))}
+                  </div>
                 ))}
-              </div>
-            ))}
-          </pre>
-        )}
-      </Highlight>
-    </div>
-    </main>
-  )
+              </pre>
+            )}
+          </Highlight>
+        </div>
+      </main>
+    </>
+  );
 }
 
-export default How
+export default How;
